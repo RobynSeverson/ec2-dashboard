94df943e7b76f4eaee1baa986d850530dd807403
Robyn Severson <robyn@robynseverson.com>
2019-12-02 02:13:46 -0800

    Add functional pagination

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { default as SampleData } from '../sampleData.json';
+
 export interface InstanceElement {
   name: string;
   type: string;
@@ -8,8 +10,6 @@ export interface InstanceElement {
   publicIP: string;
   privateIP: string;
 }
-
-
 
 const INSTANCE_DATA: InstanceElement[] = [
   {
@@ -36,17 +36,29 @@ const INSTANCE_DATA: InstanceElement[] = [
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  length = SampleData.length;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  pageEvent: PageEvent;
+  dataSource: InstanceElement[];
   constructor() { }
 
   ngOnInit() {
-    console.log(SampleData);
+    this.getPage();
   }
 
-  getRandomFromList(arr: String[]) {
-    return arr[Math.floor(Math.random() * arr.length)];
+  getPage() {
+    if (this.pageEvent) {
+      this.dataSource = SampleData.slice(this.pageEvent.pageIndex * this.pageEvent.pageSize, (this.pageEvent.pageIndex + 1) * this.pageEvent.pageSize);
+    } else {
+      this.dataSource = SampleData.slice(0, this.pageSize);
+    }
+  }
+
+  switchPage(pageEvent: PageEvent) {
+    this.pageEvent = pageEvent;
+    this.getPage();
   }
 
   displayedColumns: string[] = ['name', 'id', 'privateIP', 'publicIP', 'type', 'state', 'availabilityZone'];
-  dataSource = SampleData;
 }
