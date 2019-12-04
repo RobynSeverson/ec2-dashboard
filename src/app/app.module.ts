57f23799696aa5f76bd2e1a26fea6305b96d5a2f
Robyn Severson <robyn@robynseverson.com>
2019-12-04 01:00:44 -0800

    Autocomplete WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,11 +21,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
-import { Ec2Service } from '../services/ec2.service'; 
+import { Ec2Service } from '../services/ec2.service';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 export interface InstanceElement {
   name: string;
@@ -17,6 +20,9 @@ export interface InstanceElement {
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  myControl = new FormControl();
+  options: string[] = [];
+  filteredOptions: Observable<string[]>;
   length = 0;
   pageSize = 10;
   pageIndex = 0;
@@ -29,6 +35,11 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.getPage();
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
   }
 
   getPage() {
@@ -42,15 +53,28 @@ export class DashboardComponent implements OnInit {
 
     this.ec2Service.getInstances(this.pageSize, this.pageIndex)
     .subscribe((data : any) => {
-      this.dataSource = null;
       this.dataSource = data.page;
       this.length = data.total;
+
+      this.options = [];
+      this.dataSource.forEach((instance) => {
+        this.options.push(instance.name);
+        this.options.push(instance.id);
+        this.options.push(instance.privateIP);
+        this.options.push(instance.publicIP);
+      });
     });
   }
 
   switchPage(pageEvent: PageEvent) {
     this.pageEvent = pageEvent;
     this.getPage();
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   displayedColumns: string[] = ['name', 'id', 'privateIP', 'publicIP', 'type', 'state', 'availabilityZone'];
